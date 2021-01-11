595924784cd186c1a3b6e503ff6fedb14d104992
chehsunliu <chehsunliu@gmail.com>
2021-01-11 21:36:03 +0800

    :recycle: Enhance IO

--- workbench/wb-rust/src/main.rs
+++ workbench/wb-rust/src/main.rs
@@ -1,39 +1,40 @@
+use std::error::Error;
 use std::io;
-use std::process;
 
 // 2^31 - 1 = 2147483647
-fn solve(a: u64, b: u64) -> u64 {
-    let a_odds = (a + 1) / 2;
-    let a_evens = a / 2;
-    let b_odds = (b + 1) / 2;
-    let b_evens = b / 2;
-
-    // 10^9 * 10^9 = 10^18 = (10^3)^6 ~= (2^10)^6 = 2^60
-    a_odds * b_odds + a_evens * b_evens
+// 10^9 * 10^9 = 10^18 = (10^3)^6 ~= (2^10)^6 = 2^60
+fn solve(heights: Vec<i32>, k: i32) -> i32 {
+    0
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let stdin = io::stdin();
     let mut buffer = String::new();
 
-    stdin
-        .read_line(&mut buffer)
-        .expect("Failed to read # of cases");
+    stdin.read_line(&mut buffer)?;
 
     loop {
         buffer.clear();
-        let stdin_result = stdin.read_line(&mut buffer);
-
-        if let Err(_) = stdin_result {
-            process::exit(1);
-        } else if let Ok(0) = stdin_result {
+        if stdin.read_line(&mut buffer)? == 0 {
             break;
         }
 
         let words: Vec<&str> = buffer.trim().split_whitespace().collect();
-        let a: u64 = words[0].parse().unwrap();
-        let b: u64 = words[1].parse().unwrap();
+        let k: i32 = words[1].parse()?;
 
-        println!("{}", solve(a, b));
+        buffer.clear();
+        if stdin.read_line(&mut buffer)? == 0 {
+            break;
+        }
+
+        let heights: Vec<i32> = buffer
+            .trim()
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect();
+
+        println!("{}", solve(heights, k));
     }
+
+    Ok(())
 }
