f7ea0ac2f51a6b01bb2e9f501ec2edd4f126bcf0
chehsunliu <chehsunliu@gmail.com>
2021-01-16 21:53:34 +0800

    :bento: LeetCode - Same Tree

--- workbench/wb-cpp/wbbin/main.cpp
+++ workbench/wb-cpp/wbbin/main.cpp
@@ -3,7 +3,5 @@
 #include "wblib/wblib.hpp"
 
 int main() {
-    int ans = wb::add(3, 4);
-    printf("Hi! %d\n", ans);
     return 0;
 }
--- workbench/wb-cpp/wblib/src/wblib_tests.cpp
+++ workbench/wb-cpp/wblib/src/wblib_tests.cpp
@@ -3,8 +3,4 @@
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
 
-TEST_CASE("simple") {
-    int actual_result = wb::add(3, 4);
-
-    REQUIRE(actual_result == 7);
-}
+TEST_CASE("simple") {}
