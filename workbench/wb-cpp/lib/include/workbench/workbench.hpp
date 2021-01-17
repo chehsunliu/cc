977a0b087045a1cb6e5345272259c6570e5aa75e
chehsunliu <chehsunliu@gmail.com>
2021-01-18 01:33:50 +0800

    :art: Separate leetcode pre-defined structs

--- workbench/wb-cpp/lib/include/workbench/workbench.hpp
+++ workbench/wb-cpp/lib/include/workbench/workbench.hpp
@@ -1,24 +1,14 @@
-#ifndef __WBLIB_H__
-#define __WBLIB_H__
+#ifndef __WORKBENCH_H__
+#define __WORKBENCH_H__
 
 #include <vector>
 #include <algorithm>
 
+#include "workbench/leetcode.hpp"
+
 using namespace std;
 
 namespace wb {
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
 
 class Solution {
 public:
@@ -39,6 +29,5 @@ public:
 };
 
 }
-
 
 #endif
