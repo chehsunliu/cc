d5d5c8e3bf9ab6f3bd279f113ddb3d7bcf908638
chehsunliu <chehsunliu@gmail.com>
2021-01-18 23:36:47 +0800

    :white_check_mark: Add test cases

--- workbench/wb-cpp/lib/src/workbench_tests.cpp
+++ workbench/wb-cpp/lib/src/workbench_tests.cpp
@@ -1,19 +1,45 @@
-#include "workbench/workbench.hpp"
+#include <vector>
 
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
 
-TEST_CASE("simple") {
-    auto *obj = new wb::MinStack();
+#include "workbench/workbench.hpp"
 
-    obj->push(11);
-    obj->push(5);
-    obj->push(11);
-    obj->push(8);
-    obj->push(3);
+TEST_CASE("example1") {
+    wb::Solution sol;
 
-    obj->pop();
+    int _numbers[] = {2, 7, 11, 15};
+    std::vector<int> numbers(_numbers, _numbers + 4);
+    std::vector<int> actual_results = sol.twoSum(numbers, 9);
 
-    REQUIRE(obj->top() == 8);
-    REQUIRE(obj->getMin() == 5);
+    int _expected_results[] = {1, 2};
+    std::vector<int> expected_results(_expected_results, _expected_results + 2);
+
+    REQUIRE(actual_results == expected_results);
+}
+
+TEST_CASE("example2") {
+    wb::Solution sol;
+
+    int _numbers[] = {2, 3, 4};
+    std::vector<int> numbers(_numbers, _numbers + 3);
+    std::vector<int> actual_results = sol.twoSum(numbers, 6);
+
+    int _expected_results[] = {1, 3};
+    std::vector<int> expected_results(_expected_results, _expected_results + 2);
+
+    REQUIRE(actual_results == expected_results);
+}
+
+TEST_CASE("example3") {
+    wb::Solution sol;
+
+    int _numbers[] = {-1, 0};
+    std::vector<int> numbers(_numbers, _numbers + 2);
+    std::vector<int> actual_results = sol.twoSum(numbers, -1);
+
+    int _expected_results[] = {1, 2};
+    std::vector<int> expected_results(_expected_results, _expected_results + 2);
+
+    REQUIRE(actual_results == expected_results);
 }
