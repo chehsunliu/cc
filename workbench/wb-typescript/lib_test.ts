1468c981c6a7ee8f6dc742bf799207c95ee42916
chehsunliu <chehsunliu@gmail.com>
2021-01-21 01:35:35 +0800

    :bento: LeetCode #387 / typescript

--- workbench/wb-typescript/lib_test.ts
+++ workbench/wb-typescript/lib_test.ts
@@ -1,22 +1,11 @@
 import { assertEquals } from "https://deno.land/std@0.84.0/testing/asserts.ts";
 
-import { isUgly } from "./lib.ts";
+import { firstUniqChar } from "./lib.ts";
 
 Deno.test("GG #1", () => {
-  assertEquals(isUgly(6), true);
+  assertEquals(firstUniqChar("leetcode"), 0);
 });
 
 Deno.test("GG #2", () => {
-  assertEquals(isUgly(8), true);
-});
-
-Deno.test("GG #3", () => {
-  assertEquals(isUgly(14), false);
-});
-
-Deno.test("GG #4", () => {
-  assertEquals(isUgly(1), true);
-  assertEquals(isUgly(0), false);
-  assertEquals(isUgly(-1), false);
-  assertEquals(isUgly(288637140), false);
+  assertEquals(firstUniqChar("loveleetcode"), 2);
 });
