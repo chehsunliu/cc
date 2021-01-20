191482b6f4810d8d04a8b294ffcbe34d976b9ef7
chehsunliu <chehsunliu@gmail.com>
2021-01-20 23:48:09 +0800

    :bento: LeetCode #242 / typescript

--- workbench/wb-typescript/lib_test.ts
+++ workbench/wb-typescript/lib_test.ts
@@ -1,26 +1,11 @@
 import { assertEquals } from "https://deno.land/std@0.84.0/testing/asserts.ts";
 
-import { isPowerOfTwo } from "./lib.ts";
+import { isAnagram } from "./lib.ts";
 
 Deno.test("GG #1", () => {
-  assertEquals(isPowerOfTwo(1), true);
+  assertEquals(isAnagram("anagram", "nagaram"), true);
 });
 
 Deno.test("GG #2", () => {
-  assertEquals(isPowerOfTwo(16), true);
-});
-
-Deno.test("GG #3", () => {
-  assertEquals(isPowerOfTwo(3), false);
-});
-
-Deno.test("GG #4", () => {
-  assertEquals(isPowerOfTwo(5), false);
-});
-
-Deno.test("GG #5", () => {
-  assertEquals(isPowerOfTwo(0), false);
-  assertEquals(isPowerOfTwo(-1), false);
-  assertEquals(isPowerOfTwo(-2), false);
-  assertEquals(isPowerOfTwo(-3), false);
+  assertEquals(isAnagram("rat", "car"), false);
 });
