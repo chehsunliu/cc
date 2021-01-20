7bc0bcb54f2f0a0c7b9ef3f4bf6e5bb2ab75ca28
chehsunliu <chehsunliu@gmail.com>
2021-01-21 01:13:08 +0800

    :bento: LeetCode #263 / typescript

--- workbench/wb-typescript/lib_test.ts
+++ workbench/wb-typescript/lib_test.ts
@@ -1,11 +1,22 @@
 import { assertEquals } from "https://deno.land/std@0.84.0/testing/asserts.ts";
 
-import { isAnagram } from "./lib.ts";
+import { isUgly } from "./lib.ts";
 
 Deno.test("GG #1", () => {
-  assertEquals(isAnagram("anagram", "nagaram"), true);
+  assertEquals(isUgly(6), true);
 });
 
 Deno.test("GG #2", () => {
-  assertEquals(isAnagram("rat", "car"), false);
+  assertEquals(isUgly(8), true);
+});
+
+Deno.test("GG #3", () => {
+  assertEquals(isUgly(14), false);
+});
+
+Deno.test("GG #4", () => {
+  assertEquals(isUgly(1), true);
+  assertEquals(isUgly(0), false);
+  assertEquals(isUgly(-1), false);
+  assertEquals(isUgly(288637140), false);
 });
