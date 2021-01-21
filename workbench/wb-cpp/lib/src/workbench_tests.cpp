733a5b24d1743847fd56867235f1faf9403dfe78
chehsunliu <chehsunliu@gmail.com>
2021-01-22 01:40:37 +0800

    :bento: LeetCode #345 / c++

--- workbench/wb-cpp/lib/src/workbench_tests.cpp
+++ workbench/wb-cpp/lib/src/workbench_tests.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <string>
 
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
@@ -6,12 +7,7 @@
 #include "workbench/workbench.hpp"
 
 TEST_CASE("GG #1") {
-    int _nums[] = {-2, 0, 3, -5, 2, -1};
-    std::vector<int> nums(_nums, _nums + 6);
-    wb::NumArray *obj = new wb::NumArray(nums);
+    wb::Solution sol;
 
-    REQUIRE(obj->sumRange(0, 2) == 1);
-    REQUIRE(obj->sumRange(2, 5) == -1);
-    REQUIRE(obj->sumRange(0, 5) == -3);
-    REQUIRE(obj->sumRange(2, 3) == -2);
+    REQUIRE(sol.reverseVowels("hello") == "holle");
 }
