fff4ed01db9ecea2d22a186eb94efd49f6558df9
chehsunliu <chehsunliu@gmail.com>
2021-01-21 16:43:52 +0800

    :bento: leetcode #258 / c++

--- workbench/wb-cpp/lib/src/workbench_tests.cpp
+++ workbench/wb-cpp/lib/src/workbench_tests.cpp
@@ -6,23 +6,6 @@
 #include "workbench/workbench.hpp"
 
 TEST_CASE("GG #1") {
-    int _nums[] = {1, 2};
-    std::vector<int> nums(_nums, _nums + 2);
-    wb::ListNode *head = wb::ListNode::from_vector(nums);
-
     wb::Solution sol;
-    REQUIRE(sol.isPalindrome(head) == false);
-
-    delete head;
-}
-
-TEST_CASE("GG #2") {
-    int _nums[] = {1, 2, 2, 1};
-    std::vector<int> nums(_nums, _nums + 4);
-    wb::ListNode *head = wb::ListNode::from_vector(nums);
-
-    wb::Solution sol;
-    REQUIRE(sol.isPalindrome(head) == true);
-
-    delete head;
+    REQUIRE(sol.addDigits(38) == 2);
 }
