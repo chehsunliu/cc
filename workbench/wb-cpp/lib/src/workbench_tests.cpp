a74af231e21ccc255d03c74a7a7394f1123f1d7e
chehsunliu <chehsunliu@gmail.com>
2021-01-23 14:42:55 +0800

    :bento: LeetCode #6 / c++

--- workbench/wb-cpp/lib/src/workbench_tests.cpp
+++ workbench/wb-cpp/lib/src/workbench_tests.cpp
@@ -5,17 +5,17 @@
 TEST_CASE("GG #1") {
     wb::Solution sol;
 
-    REQUIRE(sol.longestPalindrome("abccccdd") == 7);
+    REQUIRE(sol.convert("PAYPALISHIRING", 3) == "PAHNAPLSIIGYIR");
 }
 
 TEST_CASE("GG #2") {
     wb::Solution sol;
 
-    REQUIRE(sol.longestPalindrome("a") == 1);
+    REQUIRE(sol.convert("PAYPALISHIRING", 4) == "PINALSIGYAHRPI");
 }
 
 TEST_CASE("GG #3") {
     wb::Solution sol;
 
-    REQUIRE(sol.longestPalindrome("bb") == 2);
+    REQUIRE(sol.convert("A", 1) == "A");
 }
