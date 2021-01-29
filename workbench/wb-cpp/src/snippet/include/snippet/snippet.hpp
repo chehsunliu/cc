79151d391cf6ce3f4cb185ba2cc5a724896c4c3c
chehsunliu <chehsunliu@gmail.com>
2021-01-30 00:11:08 +0800

    :bento: CodeChef KPRIME / c++

--- workbench/wb-cpp/src/snippet/include/snippet/snippet.hpp
+++ workbench/wb-cpp/src/snippet/include/snippet/snippet.hpp
@@ -6,13 +6,22 @@
 
 namespace snippet {
 
-template <class T> void print_vector(const std::vector<T> &vec) {
-    for (auto &item : vec) {
-        std::cerr << item << " ";
+template <class T> void print_vector(const std::vector<T> &vec, int n, bool reverse) {
+    if (reverse) {
+        for (int i = 0; i < n && vec.size() - 1 - i >= 0; i++) {
+            std::cerr << vec[vec.size() - 1 - i] << " ";
+        }
+    } else {
+        for (int i = 0; i < n && i < vec.size(); i++) {
+            std::cerr << vec[i] << " ";
+        }
     }
     std::cerr << std::endl;
 }
 
-}
+template <class T> void print_vector(const std::vector<T> &vec) { print_vector(vec, vec.size(), false); }
+template <class T> void print_vector(const std::vector<T> &vec, int n) { print_vector(vec, n, false); }
+
+} // namespace snippet
 
 #endif
