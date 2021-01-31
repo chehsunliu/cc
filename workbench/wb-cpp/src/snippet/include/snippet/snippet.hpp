7683b3f3615e49a932a85c0ada2a3653ece094b7
chehsunliu <chehsunliu@gmail.com>
2021-01-31 23:32:00 +0800

    :bento: CodeChef FROGV / c++

--- workbench/wb-cpp/src/snippet/include/snippet/snippet.hpp
+++ workbench/wb-cpp/src/snippet/include/snippet/snippet.hpp
@@ -6,6 +6,19 @@
 
 namespace snippet {
 
+template <class T> void print_vector(const std::vector<T*> &vec, int n, bool reverse) {
+    if (reverse) {
+        for (int i = 0; i < n && vec.size() - 1 - i >= 0; i++) {
+            std::cerr << *vec[vec.size() - 1 - i] << " ";
+        }
+    } else {
+        for (int i = 0; i < n && i < vec.size(); i++) {
+            std::cerr << *vec[i] << " ";
+        }
+    }
+    std::cerr << std::endl;
+}
+
 template <class T> void print_vector(const std::vector<T> &vec, int n, bool reverse) {
     if (reverse) {
         for (int i = 0; i < n && vec.size() - 1 - i >= 0; i++) {
