4a640507f90c0d25bf4f8145b597a972ef56171f
chehsunliu <chehsunliu@gmail.com>
2021-02-18 01:11:59 +0800

    :recycle: g

--- zz/zz-cpp/src/snippet/src/modulo_tests.cpp
+++ zz/zz-cpp/src/snippet/src/modulo_tests.cpp
@@ -2,48 +2,51 @@
 #include <string>
 
 #include "catch2/catch.hpp"
-
 #include "snippet/modulo.hpp"
 
 using namespace snippet::modulo;
 
 typedef ModuloNumber<int, 7> mint7;
 
-TEST_CASE("Small number arithmetic") {
-    SECTION("constructor") {
-        mint7 a1 = -1;
-        REQUIRE(a1 == 6);
+TEST_CASE("Small number") {
+    SECTION("Constructors") {
+        REQUIRE(mint7(-8) == 6);
+        REQUIRE(mint7(-1) == 6);
+        REQUIRE(mint7(6) == 6);
+        REQUIRE(mint7(13) == 6);
     }
 
-    SECTION("addition / +") {
+    SECTION("Comparison operators") {
+        REQUIRE(mint7(3) == mint7(10));
+        REQUIRE(mint7(3) != mint7(9));
+    }
+
+    SECTION("Immutable arithmetic operators") {
         mint7 a1 = 12, a2 = 13;
+
         REQUIRE(a1 + a2 == 4);
         REQUIRE(a1 + a2 != 5);
         REQUIRE(a1 + a2 == 25);
         REQUIRE(a1 + a2 != 26);
+
+        REQUIRE(a1 - a2 == 6);
+        REQUIRE(a2 - a1 == 1);
+
+        REQUIRE(a1 * a2 == 2);
     }
 
-    SECTION("addition / ++") {
+    SECTION("Mutable arithmetic operators / addition") {
         mint7 a1 = 12;
         REQUIRE(a1++ == 5);
         REQUIRE(a1 == 6);
         REQUIRE(++a1 == 0);
         REQUIRE(a1 == 0);
+
+        REQUIRE((a1 += 15) == 1);
+        REQUIRE((a1 += 6) == 0);
     }
 
-    SECTION("addition / +=") {
-        mint7 a1 = 12;
-        REQUIRE((a1 += 15) == 6);
-        REQUIRE((a1 += 6) == 5);
-    }
-
-    SECTION("subtraction / -") {
-        mint7 a1 = 4, a2 = 17;
-        REQUIRE(a1 - a2 == 1);
-        REQUIRE(a2 - a1 == 6);
-    }
-
-    SECTION("subtraction / --") {
+    SECTION("Mutable arithmetic operators / subtraction") {
         mint7 a1 = 4;
         REQUIRE(a1-- == 4);
         REQUIRE(a1 == 3);
@@ -52,17 +55,9 @@ TEST_CASE("Small number arithmetic") {
         REQUIRE(--a1 == 1);
         REQUIRE(--a1 == 0);
         REQUIRE(--a1 == 6);
-    }
 
-    SECTION("subtraction / -=") {
-        mint7 a1 = 4;
-        REQUIRE((a1 -= 15) == 3);
-        REQUIRE((a1 -= 6) == 4);
-    }
-
-    SECTION("multiplication") {
-        mint7 a1 = 12, a2 = 13;
-        REQUIRE(a1 * a2 == 2);
+        REQUIRE((a1 -= 15) == 5);
+        REQUIRE((a1 -= 6) == 6);
     }
 }
 
